587ad4ec5629de39e651bba5af606f62d80cb475
LiquidDark <fisch890@gmail.com>
2016-07-06 19:55:23 +0200

    Switch arm-eabi-6.0 UBERTC

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,7 +87,7 @@ BOARD_USES_GROUPER_MODULES := true
 BOARD_KERNEL_CMDLINE := androidboot.hardware=$(TARGET_BOOTLOADER_BOARD_NAME) androidboot.selinux=permissive
 TARGET_KERNEL_CONFIG := liquiddark_grouper_defconfig
 TARGET_KERNEL_SOURCE := kernel/asus/grouper
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.3/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-6.0/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
 # Changelog
